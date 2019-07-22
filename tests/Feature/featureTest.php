1d55a87c56ce172533b133c9b449d3ced5b0c8b0
prajjavalgupta <prajjwal.gupta531@gmail.com>
2019-07-21 19:50:53 -0700

    added a test for testing register page

---
+++ tests/Feature/featureTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class featureTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+
+    /**
+     *  Feature Test for register page
+     */
+    public function testExample()
+    {
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+    }
+
+}
